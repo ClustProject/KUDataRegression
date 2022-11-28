db50520b221a66849320f2893c05e3c079c685ff
KimHyeon-Ji <72793869+KimHyeon-Ji@users.noreply.github.com>
2022-11-28 22:05:55 +0900

    Update main_regression.py

--- main_regression.py
+++ main_regression.py
@@ -1,6 +1,7 @@
 import torch
 import pandas as pd
 import numpy as np
+import os
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 from models.train_model import Train_Test
@@ -139,6 +140,9 @@ class Regression():
         :param best_model_path: path for saving model
         :type best_model_path: str
         """
+        
+        # make folder to save model
+        os.makedirs('./ckpt', exist_ok=True)
 
         # save model
         torch.save(best_model.state_dict(), best_model_path)
